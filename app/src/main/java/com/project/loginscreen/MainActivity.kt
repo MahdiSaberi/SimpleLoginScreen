22a99907eb4fe75f64974ead0603d103fbb9acf0
m^tt <mahdisaberi35@gmail.com>
2026-06-26 19:23:48 +0330

    :sparkles: Created Top Section for login page

--- app/src/main/java/com/project/loginscreen/MainActivity.kt
+++ app/src/main/java/com/project/loginscreen/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.project.loginscreen.ui.screen.BackgroundShape
 import com.project.loginscreen.ui.screen.LoginScreen
 import com.project.loginscreen.ui.theme.LoginScreenTheme
 
--- app/src/main/java/com/project/loginscreen/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/project/loginscreen/ui/screen/LoginScreen.kt
@@ -4,16 +4,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.project.loginscreen.R
 import com.project.loginscreen.ui.theme.Black
 
@@ -21,19 +32,70 @@ import com.project.loginscreen.ui.theme.Black
 fun LoginScreen(modifier: Modifier = Modifier) {
     Surface {
         Column(modifier = Modifier.fillMaxSize()) {
-            val uiColor = if (isSystemInDarkTheme()) Black else Color.White
-            Box(
-                contentAlignment = Alignment.TopCenter
-            ) {
-                Image(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(fraction = 0.48f),
-                    painter = painterResource(R.drawable.shape),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillBounds,
+            TopSection()
+            Spacer(modifier = Modifier.height(16.dp))
+            BottomSection()
+        }
+    }
+}
+
+@Composable
+private fun BottomSection() {
+
+}
+@Composable
+private fun TopSection() {
+
+    val uiColor = if (isSystemInDarkTheme()) Color.White else Black
+    Box(
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 0.48f),
+            painter = painterResource(R.drawable.shape),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+        )
+
+        Row(
+            modifier = Modifier.padding(72.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier.size(64.dp),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = stringResource(id = R.string.app_name),
+                tint = uiColor
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Column {
+                Text(
+                    text = stringResource(id = R.string.banana),
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = uiColor,
+//                            fontFamily = Rubik,
+//                            fontSize = 40.sp
+                )
+                Text(
+                    text = stringResource(id = R.string.enjoy_chat),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = uiColor,
+//                            fontFamily = Roboto,
+//                            fontSize = 24.sp
                 )
             }
         }
+        Text(
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .align(alignment = Alignment.BottomCenter),
+            text = stringResource(id = R.string.login),
+            style = MaterialTheme.typography.headlineLarge,
+            color = uiColor,
+//                    fontFamily = Roboto,
+//                    fontSize = 24.sp
+        )
     }
 }
